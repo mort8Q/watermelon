97b3a56149c9efc29dce1a4d631d354e77846a49
mort8q <mort8k@hotmail.com>
2019-04-03 16:23:19 +0300

    Updates

---
+++ precache-manifest.baf921987e4139d3ae2eede179ef6895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d40ca9a31bcc2f4d7334",
+    "url": "/watermelon/static/js/runtime~main.1cf09104.js"
+  },
+  {
+    "revision": "1c325525990b361d5369",
+    "url": "/watermelon/static/js/main.d7bb79b5.chunk.js"
+  },
+  {
+    "revision": "64eb2bc77d255bba9161",
+    "url": "/watermelon/static/js/2.c54d1532.chunk.js"
+  },
+  {
+    "revision": "1c325525990b361d5369",
+    "url": "/watermelon/static/css/main.4de8ba28.chunk.css"
+  },
+  {
+    "revision": "64eb2bc77d255bba9161",
+    "url": "/watermelon/static/css/2.c830ee4a.chunk.css"
+  },
+  {
+    "revision": "b6ad73ff15ce6814287456096dfccff7",
+    "url": "/watermelon/index.html"
+  }
+];
